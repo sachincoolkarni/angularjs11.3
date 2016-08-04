27be58df334108fd78be2a61cd4eca80d6b16cab
sachincoolkarni <sachincoolkarni@users.noreply.github.com>
2016-08-04 12:27:45 +0530

    Create angular11js.js

---
+++ angular11js.js
@@ -0,0 +1,14 @@
+var mod=angular.module("myapp",[]);
+mod.controller("mycontroller",["$scope",function($scope)
+
+{
+	$scope.alert=function()
+	{
+		alert("You have clicked the button");
+	}
+
+
+
+
+
+}]);
